bcc20fba4dffe047b9195633315e814807053fbb
andrey ovcharenko <andrey.ovcharenko@branderstudio.com>
2021-12-08 00:12:27 +0200

    tag name from property

--- src/background/index.js
+++ src/background/index.js
@@ -2,16 +2,16 @@ import * as riot  from 'riot'
 import TestTag from './test.riot';
 
 console.log(TestTag);
-riot.register('test-tag', TestTag)
+riot.register(TestTag.name, TestTag)
 
 window.addEventListener('load', (event) => {
     const asins = document.querySelectorAll('[data-asin]:not([data-asin=""])');
     for (let i = 0; i < asins.length; i++) {
         const asin = asins[i];
-        const el = asin.insertBefore(document.createElement('test-tag'), asin.firstChild)
+        const el = asin.insertBefore(document.createElement('div'), asin.firstChild)
         //el.appendChild(document.createTextNode('*** test ***'))
         el.setAttribute('data-asin', asin.getAttribute('data-asin'));
-        //el.setAttribute('is', 'test-tag')
+        el.setAttribute('is', 'test-tag')
         riot.mount(el)
     }
 
