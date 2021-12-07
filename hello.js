59faf04d1402e5aa96f0a1be8bf9ddfdc99c067d
andrey ovcharenko <andrey.ovcharenko@branderstudio.com>
2021-12-07 17:14:25 +0200

    first commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', (event) => {
+    const asins = document.querySelectorAll('[data-asin]:not([data-asin=""])');
+    for (let i = 0; i < asins.length; i++) {
+        const asin = asins[i];
+        asin.insertBefore(document.createElement('div').appendChild(document.createTextNode('*** test ***')), asin.firstChild)
+    }
+}, false);
---
+++ readme.md
@@ -0,0 +1,21 @@
+# amazond-plugin
+
+
+
+## Start
+
+```
+npm start
+```
+
+## Test
+
+```
+npm test
+```
+
+## Build
+
+```
+npm run build
+```
